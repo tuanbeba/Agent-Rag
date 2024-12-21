db9a0de7ef73a60637e1412423ffca5d77835fea
tuan1608 <tuan.1608.kc@gmail.com>
2024-12-22 05:20:16 +0700

    update db vector

--- src/crawler.py
+++ src/crawler.py
@@ -8,28 +8,30 @@ import json
 def processing_text(text: str):
     # thay thế "\n\n+" thành "\n\n" và loại bỏ khoảng trắng đầu và cuối câu
     text = re.sub(r"\n\n+", "\n\n", text).strip()
+    # xóa các ký tự chứa surrogate
+    text = re.sub(r'[\ud800-\udfff]', '', text)
     return text
 
-def loadPDF_from_file(file_pdf_local: str):
+def loadPDF_from_file(path_pdf: str):
 
     #tải file fpd từ đường dẫn local
-    pdf_loader = PyPDFLoader(file_pdf_local)
+    pdf_loader = PyPDFLoader(path_pdf)
     docs = pdf_loader.load()
     for doc in docs:
         doc.page_content = processing_text(doc.page_content)
-    print(f"len document: {len(docs)}")
+    print(f"len document: {len(docs)}") # 14
     
     # chia nhỏ các document thành các chunks
     separators: List[str]  = ["\n\n", "\n", " ", ""]
-    char_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 300, separators=separators)
+    char_splitter = RecursiveCharacterTextSplitter(chunk_size = 2000, chunk_overlap = 300, separators=separators)
     docs_splitted = char_splitter.split_documents(docs)
-    print(f"len document splitted: {len(docs_splitted)}")
+    print(f"len document splitted: {len(docs_splitted)}") # 35
     return docs_splitted
 
-def save_documents(file_pdf: str, directory: str):
+def save_documents(path_pdf: str, directory: str):
 
     # tải file pdf từ đường dẫn
-    docs = loadPDF_from_file(file_pdf)
+    docs = loadPDF_from_file(path_pdf)
     # tạo dic nếu dic chưa tồn tại
     if not os.path.exists(directory):
         os.makedirs(directory)
@@ -43,9 +45,13 @@ def save_documents(file_pdf: str, directory: str):
         }
         save_data.append(data)
 
-    # save dữ liệu vào fiel json
-    file_json = os.path.join(directory,"document.json")
-    with open(file = file_json, mode="w") as f:
+    # lấy tên file pdf
+    file_pdf = os.path.split(path_pdf)[1]
+    # thay phần đuôi mở rộng pdf thành json
+    file_json = os.path.splitext(file_pdf)[0] + '.json'
+    # tạo đường dẫn tới file json
+    path_json = os.path.join(directory,file_json)
+    with open(file = path_json, mode="w") as f:
         json.dump(save_data, f, indent = 4)
 
     print("saved data")
--- src/database_vector.py
+++ src/database_vector.py
@@ -1,24 +1,128 @@
-from langchain_chroma import Chroma 
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from langchain_milvus import Milvus
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_core.documents import Document
 import json
+import os, shutil
+from uuid import uuid4
+import chromadb
+from chromadb.config import Settings
 
 
-def db_vector(json_file: str):
+def db_vector_from_local(path_json: str, CHROMA_PATH: str = "./chroma_langchain_db"):
 
     # đọc file json
-    with open(json_file, mode='r') as f:
+    with open(path_json, mode='r') as f:
         data=json.load(f)
-    print("loaded data")
+    print("loaded data from json file")
 
     # chuyển đổi sang định dạng document
     docs = []
     for doc in data:
         new_doc = Document(page_content=doc["page_content"], metadata = doc["metadata"])
         docs.append(new_doc)
+    # tạo id cho document
+    uuids = [str(uuid4()) for _ in range(len(docs))]
+    # khởi tạo model embedding
+    embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+
+    # # khởi tạo kho lưu trữ vector với Chroma (mode in-memory)
+    # vector_store = Chroma.from_documents(documents=docs, embedding=embedding_model, ids=uuids,collection_name='my_collection')
+    # # lưu các document đã embedding vào vector store
+    # print("created vector store")
+
+    # return vector_store
+
+    # khởi tạo kho lưu trữ vector với Chroma (mode in-memory save disk)
+    # vector_store = Chroma(
+    # collection_name="my_collection",
+    # embedding_function=embedding_model,
+    # persist_directory="./chroma_langchain_db",  # Where to save data locally, remove if not necessary
+    # )
+    # vector_store.add_documents(documents= docs, ids = uuids)
+
+
+
+    # # Initialization from client
+    # persistent_client = chromadb.PersistentClient()
+    # collection = persistent_client.get_or_create_collection("collection_name")
+    # collection.add(ids=["1", "2", "3"], documents=["a", "b", "c"])
+
+    # vector_store_from_client = Chroma(
+    #     client=persistent_client,
+    #     collection_name="collection_name",
+    #     embedding_function=embedding_model,
+    # )
+
+
+
+def connect_to_database(collection_name: str, persist_directory: str):
+    embedding_function= HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    vector_store = Chroma(collection_name=collection_name,
+    embedding_function=embedding_function,
+    persist_directory=persist_directory
+    )
+
+    return vector_store
+
+
 
 def main():
-    pass
+    
+    json_path = 'data/2312.16862v3.json'
+    vector_store = db_vector_from_local(json_path)
+
+    # # Lấy số lượng vector trong vector store
+    # num_vectors = vector_store._collection.count()
+    # print(f"Number of vectors in the store: {num_vectors}")
+    # # Truy xuất toàn bộ thông tin từ vector store
+    # data = vector_store._collection.get(include=["documents", "embeddings", "metadatas"])
+
+    # # Kiểm tra nếu không có data['ids'] thì truy xuất từ dữ liệu
+    # ids = data.get("ids", [])
+    # # print("Vector IDs:", ids)
+    # print(f"Number of ids in the store: {len(ids)}")
+
+    # # In các thông tin chi tiết về các vector
+    # for idx, doc_id in enumerate(ids):
+    #     print(f"ID: {doc_id}")
+    #     print(f"Document: {data['documents'][idx]}")
+    #     print(f"Embedding: {data['embeddings'][idx]}")
+    #     print(f"Metadata: {data['metadatas'][idx]}")
+    #     print("-" * 50)
+    #     break
+
+    # save and load from memory
+
+    # db_vector_from_local(json_path)
+
+    # load from disk
+    # embedding_function= HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    # vector_store = Chroma(collection_name="my_collection",
+    # embedding_function=embedding_function,
+    # persist_directory="./chroma_langchain_db")
+    # # Lấy số lượng vector trong vector store
+    # num_vectors = vector_store._collection.count()
+    # print(f"Number of vectors in the store: {num_vectors}")
+    # # Truy xuất toàn bộ thông tin từ vector store
+    # data = vector_store._collection.get(include=["documents", "embeddings", "metadatas"])
+
+    # # Kiểm tra nếu không có data['ids'] thì truy xuất từ dữ liệu
+    # ids = data.get("ids", [])
+
+    # # In các thông tin chi tiết về các vector
+    # for idx, doc_id in enumerate(ids):
+    #     print(f"ID: {doc_id}")
+    #     # print(f"Document: {data['documents'][idx]}")
+    #     print(f"Dimention of Embedding: {len(data['embeddings'][idx])}")
+    #     print(f"Metadata: {data['metadatas'][idx]}")
+    #     print("-" * 50)
+    #     break
+
+    # db_vector_from_local(json_path)
+
+        
 
 if __name__ == "__main__":
     main()
